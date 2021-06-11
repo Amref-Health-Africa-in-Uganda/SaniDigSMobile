9b33a6acf50742b977e87eb448c45c1a37051b73
mznmaurice <mznmaurice@yahoo.com>
2021-06-11 09:18:51 +0300

    Update Constants.java

--- app/src/main/java/com/tactical/sanidigsmobile/util/Constants.java
+++ app/src/main/java/com/tactical/sanidigsmobile/util/Constants.java
@@ -2,10 +2,10 @@ package com.tactical.sanidigsmobile.util;
 
 public class Constants {
 
-    public final static String PUBNUB_PUBLISH_KEY = "pub-c-c1bcbc0a-7c75-4287-b957-a1330982d9b4";
-    public final static String PUBNUB_SUBSCRIBE_KEY = "sub-c-f0b74088-2342-11eb-9c54-32dcb901e45f";
-    public final static String GOOGLE_API_KEY = "AIzaSyBywdOSXXwVBLdxCsItuII96fp9OHW3kko";
-    public final static String PUBNUB_CHANNEL_NAME = "sanidigs01_channel";
+    public final static String PUBNUB_PUBLISH_KEY = "_YOURKEY_";
+    public final static String PUBNUB_SUBSCRIBE_KEY = "_YOURKEY_";
+    public final static String GOOGLE_API_KEY = "_YOURKEY_";
+    public final static String PUBNUB_CHANNEL_NAME = "_YOURCHANNEL_";
 
     //pubnub keys and google kye here
 }
